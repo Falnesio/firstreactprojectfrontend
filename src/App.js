a49c672956ed793f21b8553a915182e2a0d0ea7a
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-03-28 20:18:52 -0300

    Finalizar aprendizado por alto dos conceitos mais importantes do React
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 // JSX (JavaScript + XML)
 
 import Header from './Header';
 
 function App() {
-  return (
-      <Header>
+    const [counter, setCounter] = useState(0);
+// Array [valor, funçãoDeAtualização]
+    function increment(){
+        setCounter(counter + 1);
+        //counter++;
+        //console.log(counter);
+    }
+
+    return (
+/*      <Header>
         Be the Hero
-      </Header> // Versão com children
+      </Header>  Versão com children*/
       //<Header title="Be the Hero" /> com propriedades
       //<Header /> versão sem propriedades
-
+      <div>
+          <Header>Contador: {counter}</Header>
+          <button onClick={increment}>Incrementar</button>
+      </div>
   );
 }
 
