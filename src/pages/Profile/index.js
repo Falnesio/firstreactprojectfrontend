dcab07e36ef2175a9fe174da4c99e2af90b07602
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-03-29 23:09:26 -0300

    Finish NewIncident mechanism Finish localstorage mechanism
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import api from '../../services/api';
@@ -13,6 +13,8 @@ export default function Profile(){
 
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
 
     useEffect(() => {
         api.get('./profile', {
@@ -42,6 +44,12 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -53,7 +61,7 @@ export default function Profile(){
                 <Link className={"button"} to="/incidents/new">
                     Cadastrar novo caso
                 </Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041"/>
                 </button>
             </header>
